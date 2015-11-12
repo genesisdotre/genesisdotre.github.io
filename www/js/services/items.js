8c4d6a4e05bbc60c8c531068efcb78afcf3a9381
Michal Stefanow <mstefanow@gmail.com>
2015-11-12 01:11:31 +0000

    Looking better.

--- www/js/services/items.js
+++ www/js/services/items.js
@@ -4,34 +4,34 @@ app.factory("itemsService", function() {
     {
       url:  "http://genesis.re/wiki",
       name: "Wiki: Main Page",
-      desc: "Entry point for genesis wiki. Main Page."
+      desc: "Entry point for the wiki."
     },    
     {
       url:  "http://genesis.re/wiki#Vision",
       name: "Wiki: Vision",
-      desc: "We are on the mission. There is a planet that needs saving."
+      desc: "We are on the mission.<br>There is a planet that needs saving."
     },
     {
       url:  "http://genesis.re/wiki#Core_Values",
       name: "Wiki: Core Values",
-      desc: "Constitution. Operating system. Basic principles."
+      desc: "Constitution.<br>Operating system.<br>Basic principles."
     },
     {
       url:  "http://genesis.re/wiki#Job",
       name: "Wiki: Consultancy",
-      desc: "Willing to work on exceptional projects to generate 10x ROI."
+      desc: "Working on exceptional projects to generate 10x ROI."
     },
 
   
     {
       url:  "http://MichalStefanow.com",
       name: "MichalStefanow.com",
-      desc: "Personal website. Some pictures and links to social media profiles."
+      desc: "Personal website.<br>Some pictures and links to social media profiles."
     },
     {
       url:  "http://iamthespecialist.com",
       name: "iamthespecialist.com",
-      desc: "Technical blog. The name is pretentious but I enjoy being controversial."
+      desc: "Technical blog.<br>The name is pretentious but I enjoy being controversial."
     },
     {
       url:  "https://mailhustle.com",
@@ -48,7 +48,7 @@ app.factory("itemsService", function() {
     {
       url:  "http://genesis.re/cruitment",
       name: "Recruitment Agency",
-      desc: "Current stage - tired of seeking co-founder - starting up on my own."
+      desc: "Taking advantage of favourable market conditions."
     },    
     {
       url:  "http://wardyworks.co.uk",
@@ -58,13 +58,13 @@ app.factory("itemsService", function() {
     {
       url:  "#equity",
       name: "Equity investment",
-      desc: "Investment in land and infrastructure. Be part of our long-term vision."
+      desc: "Land and infrastructure.<br>Be part of our long-term vision."
     },
     {
       url:  "http://genesis.re/wiki#Contact",
       name: "Contact",
       desc: "<a href='tel:+44 758 629 4279'>+44 758 629 4279</a><br>" +
-            "<a href='mailto:email@gensis.re'>email@gensis.re</a><br>" +
+            "<a href='mailto:email@genesis.re'>email@genesis.re</a><br>" +
             "Please join us on IRC and Slack channel too.."
     }
   ];
